724a749f33e60390e7160752177f6725c996f358
ManishKulal-1998 <76256792+ManishKulal-1998@users.noreply.github.com>
2020-12-29 16:56:16 +0530

    First python program
    entering the age and to determine the characteristics of that particular person

---
+++ program1.py
@@ -0,0 +1,14 @@
+age=int(input("Enter the age"))
+if age<=10:
+    print("TODDLER")
+elif age<=20:
+    print("TEENAGER")
+elif age<=60:
+    print("ADULT")
+elif age<=100:
+    print("SENIOR CITIZEN")
+else:
+    print("Cannot be determined")
+     
+    
+    
